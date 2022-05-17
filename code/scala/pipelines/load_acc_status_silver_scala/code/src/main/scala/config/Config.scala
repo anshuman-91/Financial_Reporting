f49843f4057318ad603edb8b7a9b32bf7ea93af4
anshuman@simpledatalabs.com <anshuman@simpledatalabs.com>
2022-05-17 16:20:39 +0000

    test_run_date_config

--- code/scala/pipelines/load_acc_status_silver_scala/code/src/main/scala/config/Config.scala
+++ code/scala/pipelines/load_acc_status_silver_scala/code/src/main/scala/config/Config.scala
@@ -3,4 +3,4 @@ package config
 import config.ConfigStore._
 import pureconfig._
 import io.prophecy.libs._
-case class Config(fabricName: String) extends ConfigBase
+case class Config(fabricName: String, run_date: String) extends ConfigBase
